77d62d60f5ab56ea5104d4eb296531cbe8006f71
abdulrahman-58 <160419817+abdulrahman-58@users.noreply.github.com>
2025-05-29 00:40:29 +0500

    making agent

--- advance_agent/main.py
+++ advance_agent/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from advance-agent!")
+import os
+import chainlit as cl
+from dotenv import load_dotenv
+from typing import Optional,Dict
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai"
+)
+
+model = OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=provider)
